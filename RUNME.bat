cf4a4195e3abecfc1c7d7c456d859c8c986dd541
Alexander Chang <alexanderchang208@gmail.com>
2025-12-19 15:00:39 -0600

    finish basic executable and add ocr installer

--- RUNME.bat
+++ RUNME.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set installation_error = 0
+set installation_error=0
 
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -15,15 +15,26 @@ if %errorlevel% neq 0 (
 
 python -m pip install nava
 if %errorlevel% neq 0 (
-    installation_error = 1
+    set installation_error=1
 )
 
 python -m pip install opencv-python
 if %errorlevel% neq 0 (
-    installation_error = 1
+    set installation_error=1
 )
 
-if installation_error == 1 (
+python -m pip install pytesseract Pillow
+if %errorlevel% neq 0 (
+    set installation_error=1
+)
+
+if "!installation_error!"=="1" (
     echo Installation errors and/or missing modules have prevented FRCSidewinder from working properly.
+) else (
+    set minmax=2.0
+
+    setlocal
+    cd /d %~dp0
+    python main.py
 )
 pause

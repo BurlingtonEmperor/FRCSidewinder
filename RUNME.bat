bc6641fada469f0d5f5fee0b8af5307d502bba08
Alexander Chang <alexanderchang208@gmail.com>
2025-12-19 14:45:25 -0600

    begin working on executable

---
+++ RUNME.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set installation_error = 0
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is NOT installed or not in PATH.
+    echo Installing python...
+
+    winget install -e --id Python.Python.3.13 --scope machine
+) else (
+    echo Python is installed -- Requirement met (make sure you have Python 3)
+    python --version
+)
+
+python -m pip install nava
+if %errorlevel% neq 0 (
+    installation_error = 1
+)
+
+python -m pip install opencv-python
+if %errorlevel% neq 0 (
+    installation_error = 1
+)
+
+if installation_error == 1 (
+    echo Installation errors and/or missing modules have prevented FRCSidewinder from working properly.
+)
+pause
